d7ef83500f1ee0e8f99855dca27da18afb6cbdbd
Pian <alifalpian157@gmail.com>
2025-07-25 19:18:38 +0700

    sass css

--- src/Views/auth/login/index.tsx
+++ src/Views/auth/login/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import styles from "./login.module.css";
+import styles from "./login.module.scss";
 import { useRouter } from "next/router";
 
 export default function LoginViews() {
